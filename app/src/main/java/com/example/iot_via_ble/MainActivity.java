9f38c8592cee2b5d6b9056a851a2c648ff255ba4
Ceiling_roof <ted10014@hotmail.com>
2022-01-07 23:44:00 +0800

    Committed 2021/01/07

--- app/src/main/java/com/example/iot_via_ble/MainActivity.java
+++ app/src/main/java/com/example/iot_via_ble/MainActivity.java
@@ -2,9 +2,22 @@ package com.example.iot_via_ble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startScanningButton;
+    Button stopScanningButton;
+    ListView deviceListView;
+
+    ArrayAdapter<String> listAdapter;
+    ArrayList<BluetoothDevice> deviceList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
