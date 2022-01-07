d718a12231019c63f61671843100a5ea33d935c6
Ceiling_roof <ted10014@hotmail.com>
2022-01-08 02:09:47 +0800

    Committed 2021/01/07

--- app/src/main/java/com/example/iot_via_ble/MainActivity.java
+++ app/src/main/java/com/example/iot_via_ble/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.iot_via_ble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,12 +15,22 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    // To control UI elements, we will need to access them in code.
     Button startScanningButton;
     Button stopScanningButton;
     ListView deviceListView;
 
-    ArrayAdapter<String> listAdapter;
+    // ListViews in Android are backed by adapters, which hold the data being displayed in a ListView.
+    ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
     ArrayList<BluetoothDevice> deviceList;
+
+    private static final String TAG = "MyActivity";
+
+    BluetoothManager bluetoothManager;
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothLeScanner bluetoothLeScanner;
+
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
